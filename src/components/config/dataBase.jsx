d1449feb1f2340ba2c8369f27316111b8d3f470c
jaimezzapata <zapataval2304@gmail.com>
2023-08-30 20:16:27 -0500

    Se crea proyecto inicial

---
+++ src/components/config/dataBase.jsx
@@ -0,0 +1,31 @@
+const galeria = [
+  {
+    nombre: "Bulbasaur",
+    img: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png",
+    enlace: "https://www.pokemon.com/el/pokedex/bulbasaur",
+  },
+  {
+    nombre: "Squirtle",
+    img: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png",
+    enlace: "https://www.pokemon.com/el/pokedex/squirtle",
+  },
+  {
+    nombre: "Charmander",
+    img: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png",
+    enlace: "https://www.pokemon.com/el/pokedex/charmander",
+  },
+];
+const servicios = [
+  {
+    nombre: "Entrenamiento Pokemon",
+    descripcion: "Entrenando tu pomemon inicial",
+  },
+  {
+    nombre: "Centro Pokemon",
+    descripcion: "Cuidados y mantenimiento de tu pokemon",
+  },
+];
+
+export {
+    galeria, servicios
+}
---
+++ src/components/layouts/Galeria.jsx
@@ -0,0 +1,17 @@
+import { galeria } from "../config/dataBase.jsx"
+const Galeria = () => {
+  return (
+    <section>
+        {
+            galeria.map((img)=>(
+                <section>
+                    <h1>{img.nombre}</h1>
+                    <img src={img.img} alt="" />
+                    <a href={img.enlace}>{img.nombre}</a>
+                </section>
+            ))
+        }
+    </section>
+  )
+}
+export default Galeria
